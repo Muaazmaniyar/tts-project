6f341626e3f4b834ebbf143c242fda4ae1b104bc
Aahad098 <mrahadansari098@gmail.com>
2025-09-15 14:27:16 +0530

    login complete

--- mocktest/main/urls.py
+++ mocktest/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.student_login, name='home'),
+   path('login/', views.student_login, name='login'),
     path('logout/', views.student_logout, name='logout'),
     path('start_test/', views.start_test, name='start_test'),
     path("test/", views.mcq_test, name="mcq_test"),
